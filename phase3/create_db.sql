4852fe47887d76955792b3ec85f402d8e2d28b7f
Yolo <ou.ye.canada@hotmail.com>
2015-02-15 16:59:06 -0500

    modified database

--- phase3/create_db.sql
+++ phase3/create_db.sql
@@ -3,33 +3,34 @@
 create table user (
 	id int AUTO_INCREMENT PRIMARY KEY,
 	username varchar(40) UNIQUE NOT NULL,
+	firstname varchar(40) UNIQUE NOT NULL,
+	lastname varchar(40) UNIQUE NOT NULL,
 	password varchar(100) NOT NULL,
 	created_at datetime NOT NULL,
-	
-	user_type tinyint NOT NULL,
+	email varchar(40) UNIQUE NOT NULL,
+	reputation int,
+	mailing_address
+	phone
+	gender varchar(10) UNIQUE NOT NULL,
 	admin tinyint default 0 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 create table community (
 	id int AUTO_INCREMENT PRIMARY KEY,
 	name varchar(100) UNIQUE NOT NULL,
-	
-	member_id int NOT NULL,
+	category varchar(100) UNIQUE NOT NULL,
 	FOREIGN KEY (member_id) references user(id),
-	  
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 create table project (
 	id int AUTO_INCREMENT PRIMARY KEY,
 	name varchar(100) UNIQUE NOT NULL,
+	category varchar(100) UNIQUE NOT NULL,
 	funder_id int NOT NULL,
 	initiator_id int NOT NULL,
 	created_at datetime NOT NULL,
 	description varchar(500) UNIQUE NOT NULL,
-	
-	
 	FOREIGN KEY (funder_id) references user(id),
 	FOREIGN KEY (initiator_id) references user(id),
 	funding int NOT NULL
-	
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
