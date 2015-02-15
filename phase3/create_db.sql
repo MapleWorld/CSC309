5eda6edd40dc2a9a15410299729c7bba56a940bd
Yolo <ou.ye.canada@hotmail.com>
2015-02-15 17:11:16 -0500

    modified database

--- phase3/create_db.sql
+++ phase3/create_db.sql
@@ -1,5 +1,4 @@
 -- Tested with MySQL v5.1.73
-
 create table user (
 	id int AUTO_INCREMENT PRIMARY KEY,
 	username varchar(40) UNIQUE NOT NULL,
@@ -8,9 +7,9 @@ create table user (
 	password varchar(100) NOT NULL,
 	created_at datetime NOT NULL,
 	email varchar(40) UNIQUE NOT NULL,
-	reputation int,
-	mailing_address
-	phone
+	reputation int DEFAULT 0,
+	mailing_address varchar(100) NOT NULL,
+	phone varchar(20) NOT NULL,
 	gender varchar(10) UNIQUE NOT NULL,
 	admin tinyint default 0 NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -32,5 +31,5 @@ create table project (
 	description varchar(500) UNIQUE NOT NULL,
 	FOREIGN KEY (funder_id) references user(id),
 	FOREIGN KEY (initiator_id) references user(id),
-	funding int NOT NULL
+	funding int DEFAULT 0
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
