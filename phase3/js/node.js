bb41cc8a71463ae632ccb65d9bc59a64834ec956
Yolo <ou.ye.canada@hotmail.com>
2015-02-20 06:25:09 -0500

    Working insert query for node.js

--- phase3/js/node.js
+++ phase3/js/node.js
@@ -9,9 +9,47 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
+// Selecting Query
+/*
 connection.query('SELECT * FROM user', function(err, result, fields) {
 	if (err) throw err;
 	console.log('The solution is: ', result);
 });
+*/
+
+// Need to change the user data dynamically
+/*
+var postData = {username: 'user4', 
+	firstname: 'first', 
+	lastname: 'last', 
+	password: 'password', 
+	email: 'test223@email.com'};
+*/
+	
+// Inserting Query
+/*
+var query = connection.query('INSERT INTO user SET ?', postData, function(err, result) {
+	console.log('The solution is: ', result);
+});
+console.log(query.sql);
+*/
 
 connection.end();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- phase3/test_data.sql
+++ phase3/test_data.sql
@@ -2,3 +2,10 @@
 insert into user (username, firstname, lastname, password, created_at, email, mailing_address, phone, gender) values 
 	('user1', 'first', 'last', 'password', '2014-10-23 20:04:25', 'test1@email.com', 'toronto', '416-888-8888', 'm'), 
 	('user2', 'first', 'last', 'password', '2014-10-23 20:04:25', 'test2@email.com', 'toronto', '416-888-8888', 'm');
+
+-- Test data; run this after create_db.sql
+insert into user (username, firstname, lastname, password, email) values 
+	('user1', 'first', 'last', 'password',  'test1@email.com'), 
+	('user2', 'first', 'last', 'password', 'test2@email.com');
+
+	
